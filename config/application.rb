b1c6b4d4806055cbe080f9faf13b10d35d15f117
Runidan <rudan@zlatoust.en.cx>
2022-04-28 09:51:02 +0500

    fix: changed the default locale to ru

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,11 @@ module TestGuru
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    config.time_zone = "Ekaterinburg"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = "Ekaterinburg"
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+    config.i18n.default_locale = :ru
+
   end
 end
