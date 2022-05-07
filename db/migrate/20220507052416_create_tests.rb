55bbcc060705f5611858b951fca04d2a31fac2d5
Runidan <rudan@zlatoust.en.cx>
2022-05-07 11:31:06 +0500

    fix: create migrate - add default value and null: false

--- db/migrate/20220507052416_create_tests.rb
+++ db/migrate/20220507052416_create_tests.rb
@@ -1,8 +1,8 @@
 class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
-      t.string :title, limit: 50
-      t.int :level
+      t.string :title, limit: 50, null: false
+      t.int :level, default: 0, null: false
       t.references :category, null: false, foreign_key: true
 
       t.timestamps
