60e95c2a201cf94afdecda4f9a4a52d101845fbe
Runidan <rudan@zlatoust.en.cx>
2022-05-07 11:23:49 +0500

    fix: user migrate - add null: false

--- db/migrate/20220507055942_create_users.rb
+++ db/migrate/20220507055942_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :login, limit: 20
-      t.string :password_digest
+      t.string :login, limit: 20, null: false
+      t.string :password_digest, limit: 30, null: false
 
       t.timestamps
     end
