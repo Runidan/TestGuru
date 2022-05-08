67cbebe3b467b60c604083196a7a55acaea27627
Runidan <rudan@zlatoust.en.cx>
2022-05-08 11:34:30 +0500

    feat: add author field (reference to user)

--- db/migrate/20220507052416_create_tests.rb
+++ db/migrate/20220507052416_create_tests.rb
@@ -2,7 +2,8 @@ class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
       t.string :title, limit: 50, null: false
-      t.int :level, default: 0, null: false
+      t.integer :level, default: 0, null: false
+      t.references :user, null: false, foreign_key: true
       t.references :category, null: false, foreign_key: true
 
       t.timestamps
