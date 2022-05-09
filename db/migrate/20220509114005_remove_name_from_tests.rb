c56c80a9f4e8dfb1373aa932fc4571b34bf6f7be
Runidan <rudan@zlatoust.en.cx>
2022-05-09 16:50:14 +0500

    fix: changed name colunm from user_id to author_id in tests table

---
+++ db/migrate/20220509114005_remove_name_from_tests.rb
@@ -0,0 +1,7 @@
+class RemoveNameFromTests < ActiveRecord::Migration[6.1]
+  def change
+    remove_index :tests, :user_id
+    remove_column :tests, :user_id, :string
+    add_reference :tests, :author, references: :users, index: true
+  end
+end
