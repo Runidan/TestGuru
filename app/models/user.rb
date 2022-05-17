573fd13584b25d345b5b8d8387886a525f15d24f
Runidan <rudan@zlatoust.en.cx>
2022-05-17 21:59:25 +0500

    fix: delete interpolation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   def tests(level)
-    Test.joins("INNER JOIN records ON records.test_id = tests.id AND records.user_id = #{self.id} AND tests.level = #{level}")
+    Test.joins("INNER JOIN records ON records.test_id = tests.id AND records.user_id = :id AND tests.level = :level", id:self.id,  level:level)
   end
 end
