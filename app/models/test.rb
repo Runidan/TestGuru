9a21d7cf7287bc007369653ab1e940588bd95157
Runidan <rudan@zlatoust.en.cx>
2022-05-18 10:51:21 +0500

    feat: add Test.titles(caterogy) method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.titles(category)
+    category_id = Category.select(:id).where("title = :title", title:category).first
+    Test.where(category_id:category_id).order(title: :asc).select(:title)
+  end
 end
