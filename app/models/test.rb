6e2513adad1f183a3e33a9462b21fb815953661b
Runidan <rudan@zlatoust.en.cx>
2022-05-20 14:57:50 +0500

    fix: Small change in Test model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 class Test < ApplicationRecord
   def self.get_test_titles_of_category(category)
-    Test
-      .joins("INNER JOIN categories ON categories.id = tests.category_id")
-      .where(categories: {title: category})
+    joins('INNER JOIN categories ON categories.id = tests.category_id')
+      .where(categories: { title: category })
       .order(title: :asc)
       .pluck(:title)
   end
