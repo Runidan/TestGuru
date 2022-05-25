a34ff4b47d2be021b252fe399907998c7ff16d9d
Runidan <rudan@zlatoust.en.cx>
2022-05-25 13:01:39 +0500

    fix: added dependent: :destroy to all has_*

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :made_tests, class_name: 'Test', foreign_key: :author_id
-  has_many :tests_users
-  has_many :tests, through: :tests_users
+  has_many :made_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
+  has_many :tests_users, dependent: :destroy
+  has_many :tests, through: :tests_users, dependent: :destroy
 
   def get_tests_from_level(level)
     tests.where(level:)
