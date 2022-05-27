50fd666e585b4de51469a7997b6cdc63b1513262
Runidan <rudan@zlatoust.en.cx>
2022-05-27 11:43:08 +0500

    feat: add 3 level scopes

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  scope :simple_lvl, -> { where(level: 0..1)}
+  scope :middle_lvl, -> { where(level: 2..4)}
+  scope :hard_lvl, -> { where(level: 5..Float::INFINITY)}
+
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
