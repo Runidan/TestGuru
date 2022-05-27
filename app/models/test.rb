cdabfa98c487aedf26542373fb892658a360cf06
Runidan <rudan@zlatoust.en.cx>
2022-05-27 14:47:55 +0500

    feat: added validates :level

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
-  scope :simple_lvl, -> { where(level: 0..1)}
-  scope :middle_lvl, -> { where(level: 2..4)}
-  scope :hard_lvl, -> { where(level: 5..Float::INFINITY)}
-
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
@@ -12,10 +8,16 @@ class Test < ApplicationRecord
   has_many :tests_users, dependent: :destroy
   has_many :users, through: :tests_users, dependent: :destroy
 
+  validates :title, presence: true
+  validates :level, numericality: { only_integer: true, greater_than: 0 }
+
+  scope :simple_lvl, -> { where(level: 0..1) }
+  scope :middle_lvl, -> { where(level: 2..4) }
+  scope :hard_lvl, -> { where(level: 5..Float::INFINITY) }
+  scope :by_level, ->(level) { where(level:) }
+  scope :by_category, ->(category) { joins(:category).where(category: { title: category }).order(title: :asc) }
+
   def self.get_test_titles_of_category(category)
-    joins(:category)
-      .where(categories: { title: category })
-      .order(title: :asc)
-      .pluck(:title)
+    by_category(category).pluck(:title)
   end
 end
