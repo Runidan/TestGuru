ded1a5b95768ce3a38fe4e53a0b98df4c5bfdb91
Runidan <rudan@zlatoust.en.cx>
2022-05-28 12:45:21 +0500

    fix: added private modificator to valid_amount

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -8,6 +8,8 @@ class Answer < ApplicationRecord
   validates :body, presence: true
   validate :valid_amount, on: %i[save save! create create! valid? invalid?]
 
+  private
+
   def valid_amount
     errors.add(:amount_answers, 'Amount of anwers to one question must be less 5') if question.answers.count > 5
   end
