6caa06f69b11c187e1a76180ffb4fc3e89bef83d
Runidan <rudan@zlatoust.en.cx>
2022-05-28 12:31:45 +0500

    fix: moved association to the top

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,13 +3,13 @@
 class Answer < ApplicationRecord
   scope :correct, -> { where(correct: true) }
 
+  belongs_to :question
+
   validates :body, presence: true
   validate :valid_amount
 
   def valid_amount
     errors.add(:amount_answers, "Amount of anwers to one question must be less 5") if self.question.answers.count > 5
   end
-
-  belongs_to :question
 
 end
