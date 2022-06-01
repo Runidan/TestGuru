2dca65deb98b02e59f60e1c9823d721b614cbd50
Runidan <rudan@zlatoust.en.cx>
2022-06-02 01:11:29 +0500

    feat: made homework

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class QuestionsController < ApplicationController
-  before_action :get_test, only: [:index, :show]
-  before_action :get_question, only: [:show]
+  before_action :get_test, only: %i[index show new create]
+  before_action :get_question, only: %i[show destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
     render inline: '<ul><% @test.questions.each do |question| %><li><%= question.body %></li><% end %></ul>'
@@ -12,17 +14,25 @@ class QuestionsController < ApplicationController
     render inline: '<p><%= @question.body %></p>'
   end
 
-  def new
-    
-  end
+  def new;  end
 
   def edit; end
 
-  def create; end
+  def create
+    @question = Question.new question_paras
+    if @question.save
+      redirect_to test_questions_path
+    else
+      redirect_to new_test_question_path
+    end
+  end
 
   def update; end
 
-  def destroy; end
+  def destroy
+    @question.destroy
+    redirect_to test_questions_path(@question.test_id)
+  end
 
   private
 
@@ -32,5 +42,13 @@ class QuestionsController < ApplicationController
 
   def get_question
     @question = Question.find(params[:id])
+  end
+
+  def question_paras
+    params.require(:question).permit(:body, :test_id)
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question was not found'
   end
 end
