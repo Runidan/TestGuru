4c9703478e2a822d1480d20b3d52d7b53a100732
Runidan <rudan@zlatoust.en.cx>
2022-06-05 17:33:46 +0500

    feat: added tests controller through migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'tests/new'
+  get 'tests/create'
+  get 'tests/edit'
+  get 'tests/update'
+  get 'tests/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :tests do
