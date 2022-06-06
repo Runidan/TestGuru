0440a73bfc80bf2e41f7273d394f3aabd4745b87
Runidan <rudan@zlatoust.en.cx>
2022-06-06 09:40:42 +0500

    feat: added create new test form

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,21 +1,33 @@
 class TestsController < ApplicationController
 
   def index
+    @tests = Test.all
   end
-  
-  def new
 
+  def show
+    @test = Test.find(params[:id])
+  end
+
+  def new
+    @test = Test.new
   end
 
   def create
+    @test = Test.new test_params 
+
+    if @test.save
+      redirect_to @test
+
+    else
+      render :new
+    end
+
   end
 
-  def edit
+  private
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
   end
 
-  def update
-  end
-
-  def delete
-  end
 end
