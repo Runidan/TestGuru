949dc4068599e0b7f93e4719b7171e3a31a24516
Runidan <rudan@zlatoust.en.cx>
2022-06-07 00:31:24 +0500

    feat: added footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+  def current_year
+    DateTime.now.year
+  end
+
+  def github_url(author, repo)
+    link_to author, repo, target: "_blank"
+  end
 end
