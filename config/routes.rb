250a85f1831ce23a5f53e160b99cfe2645dee0cf
Runidan <rudan@zlatoust.en.cx>
2022-08-31 15:12:38 +0500

    feat: added answers scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :tests do
     resources :questions, shallow: true, except: [:index]
+      resources :answers, shallow: true
   end
 end
