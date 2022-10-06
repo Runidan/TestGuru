c07c35b2e122997e97f58a76472da736c6dc37f4
Runidan <rusin.dn+github@gmail.com>
2022-10-06 10:59:45 +0500

    feat: added authenticate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'digest/sha1'
 
 class User < ApplicationRecord
   has_secure_password
@@ -15,5 +16,15 @@ class User < ApplicationRecord
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
+  end
+
+  def authenticate(password_string)
+    digest(password_string) == self.password_digest ? self : false
+  end
+
+  private
+
+  def digest(string)
+    Digest::SHA1.hexdigest(string)
   end
 end
