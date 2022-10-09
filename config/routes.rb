7e97ebe86350900590d4845103d5e3fa9b180064
Runidan <rusin.dn+github@gmail.com>
2022-10-09 15:30:00 +0500

    fix: user changed to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'user/new'
+  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root 'tests#index'
+
+  get :singup, to: 'users#new'
+  
+  resources :users, only: :create
+  
   resources :tests do
     resources :questions, shallow: true, except: :index do
       resources :answers, shallow: true, exept: :index
