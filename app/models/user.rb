85a91b339d833db96518ef1895a3c54f6d51d4de
Runidan <rusin.dn+github@gmail.com>
2022-10-09 16:07:03 +0500

    feat: after lesson 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,14 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
-  attr_reader :password
-  attr_writer :password_confirmation
 
-  has_secure_password
+  include Auth
 
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages, dependent: :destroy
   has_many :made_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
 
-  validates :login, :email, presence: true
+  has_secure_password
 
   def get_tests_from_level(level)
     tests.by_level(level)
@@ -19,20 +17,5 @@ class User < ApplicationRecord
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
-  end
-
-  def authenticate(password_string)
-    digest(password_string) == self.password_digest ? self : false
-  end
-
-  def password=(password_string)
-    @password = password_string
-    self.password_digest = digest(password_string)
-  end
-
-  private
-
-  def digest(string)
-    Digest::SHA1.hexdigest(string)
   end
 end
