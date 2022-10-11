607fb6610e2610816a351606d55f37ebe94e5c50
Runidan <rusin.dn+github@gmail.com>
2022-10-11 22:53:35 +0500

    feat: added routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'tests#index'
 
   get :singup, to: 'users#new'
+  get :login, to: 'session#new'
   
   resources :users, only: :create
+  resources :sessions, only: :create
   
   resources :tests do
     resources :questions, shallow: true, except: :index do
