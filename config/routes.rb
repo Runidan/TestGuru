7082d737fde417138b68d9bed35d5df7efb454fd
Runidan <rusin.dn+github@gmail.com>
2022-10-12 00:45:19 +0500

    fix error

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   root 'tests#index'
 
   get :singup, to: 'users#new'
-  get :login, to: 'session#new'
+  get :login, to: 'sessions#new'
   
   resources :users, only: :create
   resources :sessions, only: :create
