cd24b417c755dc5aaf491eeb04dd2f7fc6890c53
Runidan <rusin.dn+github@gmail.com>
2022-10-12 13:16:28 +0500

    feat: after video lessons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  private
+
+  def authenticate_user!
+    unless current_user
+      redirect_to login_path
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def loggin_in?
+    current_user.present?
+  end
 end
