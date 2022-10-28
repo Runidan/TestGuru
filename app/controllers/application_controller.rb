c5af1070fbd16781bb69c76fe90f7d12203b1298
Runidan <rusin.dn+github@gmail.com>
2022-10-28 20:48:34 +0500

    feat: added redirect to admin_tests_path for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,15 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:login])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:login])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:login, :first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+
+  def after_sign_in_path_for(resource)
+    if current_user.is_a?(Admin)
+      admin_tests_path
+    else
+      stored_location_for(resource) || root_path
+    end
   end
 end
