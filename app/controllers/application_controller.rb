22952ff70f28f49d301d4502220c5adad83062fb
Runidan <rusin.dn+github@gmail.com>
2022-10-29 21:41:13 +0500

    feat: deleted custom session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,14 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
-  helper_method :after_login
+  # helper_method :after_login
 
   private
 
-  def after_login(user)
-    session[:user_id] = user.id
-    redirect_to cookies.delete(:request_url) || root_path, notice: "Welcome, #{current_user.first_name} Guru"
-  end
+  # def after_login(user)
+  #   session[:user_id] = user.id
+  #   redirect_to cookies.delete(:request_url) || root_path, notice: "Welcome, #{current_user.first_name} Guru"
+  # end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[login first_name last_name])
@@ -22,6 +22,7 @@ class ApplicationController < ActionController::Base
     if current_user.is_a?(Admin)
       admin_tests_path
     else
+      flash[:notice] = "Welcome, #{current_user.first_name} Guru"
       stored_location_for(resource) || root_path
     end
   end
