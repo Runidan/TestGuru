b3c58477bf559a305219280663637841d5c12e3e
Runidan <rusin.dn+github@gmail.com>
2022-11-05 22:35:13 +0500

    feat: after rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,11 +6,12 @@ module ApplicationHelper
   end
 
   def github_url(author, repo)
-    link_to 'Repository on GitHub', "https://github.com/#{author}/#{repo}", target: '_blank', rel: 'noopener', class: 'text-decoration-none'
+    link_to 'Repository on GitHub', "https://github.com/#{author}/#{repo}", target: '_blank', rel: 'noopener',
+                                                                            class: 'text-decoration-none'
   end
 
   def flash_message(type)
-    style_alert = type == "notice" ? "alert alert-success" : "alert alert-danger" 
-    content_tag :div, flash[type], class: style_alert, role: "alert" if flash[type]
+    style_alert = type == 'notice' ? 'alert alert-success' : 'alert alert-danger'
+    content_tag :div, flash[type], class: style_alert, role: 'alert' if flash[type]
   end
 end
