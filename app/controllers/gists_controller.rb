7f8355a5982c11861ade495612f5fdf4c31560ba
Runidan <rusin.dn+github@gmail.com>
2022-11-19 16:14:36 +0500

    feat: creating gist from user

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -1,7 +1,22 @@
 
-class GistController < ApplicationController
+class GistsController < ApplicationController
+  before_action :set_test_passage
 
   def create
+    result = GistQuestionService.new(@test_passage.current_question).call
 
+    flash_options = if result.nil?
+      { alert: t('.failure') }
+    else
+      { notice: t('.success') }
+    end
+    
+    redirect_to @test_passage, flash_options
+  end
+
+  private
+
+  def set_test_passage
+    @test_passage = TestPassage.find(params[:id])
   end
 end
--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -8,18 +8,6 @@ class TestPassagesController < ApplicationController
 
   def result; end
 
-  def gist
-    result = GistQuestionService.new(@test_passage.current_question).call
-
-    flash_options = if result.success?
-      { notice: t('.success') }
-    else
-      { alert: t('.failure') }
-    end
-    
-    redirect_to @test_passage, flash_options
-  end
-
   def update
     @test_passage.accept!(params[:answer_ids])
 
