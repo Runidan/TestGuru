62f3fb4e03551627404e520842e9e076b59e4031
Runidan <rusin.dn+github@gmail.com>
2022-12-02 16:31:14 +0500

    fix default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "TestGuru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_option = { host: 'testguruweb.onrender.com'}
+  config.action_mailer.default_url_options = { host: 'testguruweb.onrender.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:                  'smtp.mail.ru',
