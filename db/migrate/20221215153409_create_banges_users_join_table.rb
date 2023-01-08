68fc0080c6c4a13c2eaa9d60c9175a5d6889ea06
Runidan <rusin.dn+github@gmail.com>
2023-01-08 10:32:21 +0500

    fix rubocop

--- db/migrate/20221215153409_create_banges_users_join_table.rb
+++ db/migrate/20221215153409_create_banges_users_join_table.rb
@@ -1,7 +1,7 @@
 class CreateBangesUsersJoinTable < ActiveRecord::Migration[6.1]
   def change
     create_join_table :badges, :users do |t|
-      t.index [:badge_id, :user_id]
+      t.index %i[badge_id user_id]
     end
   end
 end
