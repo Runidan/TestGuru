43dceff152fee394d076546c785c65812057a664
Runidan <rusin.dn+github@gmail.com>
2023-01-09 20:37:20 +0500

    feat: added validation

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,4 +1,7 @@
 class Badge < ApplicationRecord
   has_and_belongs_to_many :user
 
+  validates :badge_type, :image, :value, presence: true
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :discription, length: { maximum: 240 }
 end
