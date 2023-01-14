0c7e0153a46f2fa959a354a53d24995555dca981
Runidan <rusin.dn+github@gmail.com>
2023-01-14 14:13:48 +0500

    fix: mistake

--- app/services/rules/all_in_category_rule.rb
+++ app/services/rules/all_in_category_rule.rb
@@ -3,8 +3,10 @@
 module Rules
   class AllInCategoryRule < Rules::AbstractRule
     def satisfied_by?
-      all_test_in_category = Category.find_by(id: @test.category_id).tests.ids # ids тестов нужной категории
+      all_test_in_category = Category.find_by!(id: @badge.value).tests.ids # ids тестов нужной категории
       (all_test_in_category - new_passed_test).empty?
+    rescue ActiveRecord::RecordNotFound
+      false
     end
   end
 end
