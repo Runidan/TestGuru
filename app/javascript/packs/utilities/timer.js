2d0d17a91a3b0b234402addafd7014dd1f978a85
Runidan <rusin.dn+github@gmail.com>
2023-01-24 23:16:25 +0500

    fix mistaks

--- app/javascript/packs/utilities/timer.js
+++ app/javascript/packs/utilities/timer.js
@@ -1,11 +1,11 @@
 document.addEventListener('turbolinks:load', () => {
-  console.log('in!')
   const timer_box = document.getElementById('timer')
   let time = timer_box.dataset.lefttime
+  console.log(time)
   let x = setInterval(() => {
-    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((time % (1000 * 60)) / 1000);
+    const hours = Math.floor((time % (60 * 60 * 24)) / (60 * 60));
+    const minutes = Math.floor((time % (60 * 60)) / (60));
+    const seconds = Math.floor((time % (60)) / 1);
 
     timer_box.innerHTML = hours + ":" + minutes + ":" + seconds
     time -= 1 
